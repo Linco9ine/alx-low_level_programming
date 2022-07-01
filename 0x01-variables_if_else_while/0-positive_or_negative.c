ae48e41887e04eb9eb01417f3968623a32584ab9
Linco9ine <robeltilahun169@gmail.com>
2022-07-01 15:21:02 +0000

    if

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,8 +4,9 @@
 #include <stdio.h>
 /* betty style doc for function main goes there */
  /**
- * main - Entry point
- * Return: Always 0 (Success)
+ * main-Entry point
+ * @void-no arrangement 
+ * Return:Always 0 (Success)
  */
 int main(void)
 {
