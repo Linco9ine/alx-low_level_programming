e589f65371c3cd60c0e909860dddc2c4ec757716
Linco9ine <robeltilahun169@gmail.com>
2022-07-20 15:15:53 +0000

    rec

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,17 +1,15 @@
 #include "main.h"
 
 /**
- * @puts: writes the string s and a trailing newline to stdout.
+ * _puts_recursion: writes the string s and a trailing newline to stdout.
  *  return 0
  */
 void _puts_recursion(char *s)
 {
-    if (*s)
-    {
-        _putchar(*s);
-        _puts_recursion(s + 1);
-    }
-
-    else
-        _putchar('\n');
+if (*s)
+{
+_putchar(*s);
+_puts_recursion(s + 1);
+}
+else _putchar('\n');
 }
