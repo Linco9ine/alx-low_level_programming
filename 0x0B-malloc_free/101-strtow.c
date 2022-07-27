7bd3c9569caae501fc0ba805a6d5fab3df1216a2
Linco9ine <robeltilahun169@gmail.com>
2022-07-27 03:00:08 +0000

    mall

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -61,7 +61,9 @@ char **strtow(char *str)
 {
 	char **strings;
 	int index = 0, words, w, letters, l;
-if (str == NULL || str[0] == '\0')
+
+if(str == NULL || str[0] == '\0')
+
 		return (NULL);
 
 	words = count_words(str);
