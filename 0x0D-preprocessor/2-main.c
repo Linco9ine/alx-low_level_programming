9dfd76cae1271612ad17a1fad14127f1e50b6f52
Linco9ine <robeltilahun169@gmail.com>
2022-08-01 18:44:39 +0300

    macro

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - prints the name of the file of the program was compiled from, followed by a new line.
+ * main - prints the name of the file of the program
+ * was compiled from, followed by a new line.
  *
- * Return: always 0.
+ * Return: Always 0.
  */
 int main(void)
 {
-	printf("%s\n", __FILE__);
+	printf("%s\n",__FILE__);
 
-	return(0);
+	return (0);
 }
-
-
