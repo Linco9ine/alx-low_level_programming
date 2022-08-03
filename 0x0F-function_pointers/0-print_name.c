9b8d21bb6e791590efa0100a96084ffd2f6337c0
Linco9ine <robeltilahun169@gmail.com>
2022-08-04 00:08:08 +0300

    ptofun

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * print_name - prints name
+ * @name: the nae to be printed.
+ * @f: a pointer to a function that prints a 	name.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == || f == NULL)
+		return;
+	f(name);
+}
+
+
+	
