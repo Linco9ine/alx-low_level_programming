aaea1d693a4c5da574748229468b768025811443
Linco9ine <robeltilahun169@gmail.com>
2022-08-04 00:42:47 +0300

    ptofun

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,9 +1,11 @@
+#include <stdio,h>
 #include "function_pointers.h"
 
 /**
- * print_name - prints name
+ * print_name - prints name.
  * @name: the nae to be printed.
  * @f: a pointer to a function that prints a name.
+ * Return: void 
  */
 void print_name(char *name, void (*f)(char *))
 {
