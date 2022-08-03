2e3c1f9abd42057317ccab03812e8591830ef0f9
Linco9ine <robeltilahun169@gmail.com>
2022-08-04 00:36:17 +0300

    ptofun

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "function_pointers.h"
 
 /**
  * print_name - prints name
@@ -7,7 +7,7 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == || f == NULL)
+	if (name == NULL || f == NULL)
 		return;
 	f(name);
 }
